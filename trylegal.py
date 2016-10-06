fa1142fb0e4855ebdcdfa9d33bbd8d0b93f9b864
Asher Wood <awood5659@gmail.com>
2016-10-06 00:03:16 -0700

    trylegal.py complete
    it can load data, select a column from the data, and find the number of rows in the data

--- trylegal.py
+++ trylegal.py
@@ -3,15 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-dataFile = os.path.join(os.getcwd(), os.path.normpath("data/TRILEGAL.table"))	# File location
-
-
 # Imports the TRILEGAL data file as a table
-def load():
+def load_info():
+	dataFile = os.path.join(os.getcwd(), os.path.normpath("data/TRILEGAL.table")) # File location
 	info = pd.read_table(dataFile, delim_whitespace=True)
 	return info
 
 # returns a specific comlumn (string)
-def infocolumn(column):
-	info = load()
+def info_col(column):
+	info = load_info()
 	return info[[column]]
+
+# returns number of rows in dataset
+def info_len():
+	info = load_info()
+	return len(info.index)
+# if run as script, do something
+if __name__ == '__main__':
+	df = load_info()
+	print df
