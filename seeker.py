5beac06e35acfdc47f6d8403629051b5bdf7b802
nedwardsthacher <nedwards@thacher.org>
2016-11-10 14:34:22 -0800

    updated version still needs a lot of work

--- seeker.py
+++ seeker.py
@@ -3,6 +3,8 @@ import numpy as np
 import scipy as sp
 from astropy.io import fits
 from astropy.stats import sigma_clipped_stats
+# talk to doc swift about this
+#from photutils import DAOStarFinder
 from photutils import daofind
 from photutils import CircularAperture
 from astropy.visualization import SqrtStretch
@@ -15,32 +17,33 @@ path = "/Users/nickedwards/Desktop/"
 filename = path + "stars.fits"
 
 # some standard variables
-siglo=3
-sighi=7
-med = np.median(image)
-sig = rb.std(image)
-vmin = med - siglo*sig
-vmax = med + sighi*sig
 
-def loadImage(filename,plot=False):
+
+
+def loadImage(filename,plot=True):
 	image,header = fits.getdata(filename,0,header=True)
-	return image,header
+	med = np.median(image)
+	sig = rb.std(image)
+	siglo=3
+	sighi=7
+	vmin = med - siglo*sig
+	vmax = med + sighi*sig
 
-def plotImage(filename):
-	image = loadImage(filename)
-	plt.ion()
-	plt.figure(1)
-	plt.clf()
-	plt.imshow(image,vmin=vmin,vmax=vmax,cmap='gray')
+	if plot:
+		plt.ion()
+		plt.figure()
+		plt.clf()
+		plt.imshow(image,vmin=vmin,vmax=vmax,cmap='gray')
 
-def findSources(filemane,plot=False):
-	image,header = loadImage(filename)
-	sources = daofind(image - med, fwhm=3.0, threshold=5.*sig)
+	return image,header,med,sig
+
+def findSources(filename,plot=True):
+	image,header,med,sig = loadImage(filename,plot=plot)
+	sources = daofind(image - med, fwhm=3.0/.61, threshold=2*sig,sharphi=.6)
 	positions = (sources['xcentroid'], sources['ycentroid'])
 	apertures = CircularAperture(positions, r=4.)
 	#norm = ImageNormalize(stretch=SqrtStretch())
 	if plot:
-		plotImage(filename)
 		apertures.plot(color='blue', lw=1.5, alpha=0.5)
 
 	return sources
