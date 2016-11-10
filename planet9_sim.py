fcdc7976d0a107bfde8bb16efcefaa45df179bd3
jswift <jswift@thacher.org>
2016-11-09 20:48:33 -0800

    Started new function to write a sequence of fits files for given planet9 parameters. This will serve to generate images for the automated finder script.

--- planet9_sim.py
+++ planet9_sim.py
@@ -234,7 +234,7 @@ def make_field(size=2048,x=None,y=None,oversamp=10,bias=500,readnoise=20,seeing=
     return image
 
 
-def planet9_sequence(size=2048,oversamp=10,bias=500,readnoise=20,seeing=3.0,
+def planet9_movie(size=2048,oversamp=10,bias=500,readnoise=20,seeing=3.0,
                      plate_scale=0.61,width=10.0,background=21.3,mzp=22.5,exptime=1800.0,
                      write=False,p9pos=[1024,1024],p9mag=23.0,dpos=30.0,angle=225.0,nimage=4,
                      filename='P9',fps=2):
@@ -262,6 +262,28 @@ def planet9_sequence(size=2048,oversamp=10,bias=500,readnoise=20,seeing=3.0,
     os.system("rm p9_image*png")
 
 
+def planet9_sequence(size=2048,oversamp=10,bias=500,readnoise=20,seeing=3.0,
+                     plate_scale=0.61,width=10.0,background=21.3,mzp=22.5,exptime=1800.0,
+                     write=False,p9pos=[1024,1024],p9mag=23.0,dpos=30.0,angle=225.0,nimage=4,
+                     filename='P9'):
+    # get locations of stars
+    x, y = distribute(size=size*oversamp)
+
+    # get locations of P9
+    t = np.arange(0,dpos*nimage,dpos)
+    p9_x = p9pos[0]*oversamp + t*np.cos(np.radians(angle))*oversamp/plate_scale
+    p9_y = p9pos[1]*oversamp + t*np.sin(np.radians(angle))*oversamp/plate_scale
+
+    for i in range(nimage):
+        image = make_field(size=size,x=x,y=y,oversamp=oversamp,bias=bias,readnoise=readnoise,seeing=seeing,
+                           plate_scale=plate_scale,width=width,background=background,mzp=mzp,exptime=exptime,
+                           write=write,p9pos=[p9_x[i],p9_y[i]],p9mag=p9mag)
+
+        # write image to FITS
+        
+    return
+
+        
 # run
 if __name__ == '__main__':
-    planet9_sequence()
+    planet9_movie()
